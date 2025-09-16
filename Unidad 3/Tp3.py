441972375e8420db009dbd92881b37295cc5812c
AleSalta <alejandro.d.sanchez2015@gmail.com>
2025-09-16 16:20:27 -0300

    Unidad 3

---
+++ Unidad 3/Tp3.py
@@ -0,0 +1,156 @@
+"""
+1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años, deberá mostrar un mensaje en pantalla que 
+diga “Es mayor de edad”.
+
+edad= int(input("Ingrese su edad: "))
+if edad == 18:
+    print("Es mayor de edad")
+else:
+    print("Usted todavia en menor de edad")
+print("Hasta luego")
+"""
+"""
+2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá mostrar por pantalla un mensaje que diga
+“Aprobado”; en caso contrario deberá mostrar el mensaje “Desaprobado”.
+
+nota= float(input("Ingrese su calificaciòn: "))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
+"""
+"""
+3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un número par, imprimir por en pantalla el mensaje
+"Ha ingresado un número par"; en caso contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso 
+del operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+numero= int(input("Ingrese un nùmero entero: "))
+if numero % 2 == 0 :
+    print("Es un numero par")
+else:
+    print("Es impar")
+"""
+"""
+4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece:
+● Niño/a: menor de 12 años. 
+● Adolescente: mayor o igual que 12 años y menor que 18 años. 
+● Adulto/a joven: mayor o igual que 18 añosy menor que 30 años. ● Adulto/a: mayor o igual que 30 años. 
+
+try:
+    edad= int(input("Ingrese su edad: "))
+    if edad >= 0 and edad < 12:
+        print("Es menor de edad")
+    elif edad >= 12 and edad < 18:
+        print("Es adolescente")
+    elif edad >= 18 and edad < 30:
+        print("Es un adulto joven")
+    else:
+        print("usted es un adulto")
+except ValueError:
+    print("Eso no es un nùmero valido")
+"""
+"""
+5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres (incluyendo 8 y 14).
+Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en pantalla el mensaje 
+"Ha ingresado una contraseña correcta"; en caso contrario, imprimir por pantalla "Por favor, ingrese una
+contraseña de entre 8 y 14 caracteres". Nota: investigue el uso de la función len() en Python para evaluar
+la cantidad de elementos que tiene un iterable tal como una lista o un string.
+
+passw= input("Ingrese su contraseña: ")
+if len(passw) > 8 and len(passw) <= 14:
+    print("Ha ingresado una contraseña valida")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")   
+"""
+"""
+6) Teniendo en cuenta lo antes mencionado, escribir un programa que tome la lista numeros_aleatorios, calcule su moda, su mediana y su media y las compare para determinar si
+hay sesgo positivo, negativo o no hay sesgo. Imprimir el resultado por pantalla.
+
+import random
+from statistics import mode,median,mean
+
+numeros_aleatorios =[random.randint(1,100) for i in range(50)]
+print("Moda",mode(numeros_aleatorios))
+print("Mediana",median(numeros_aleatorios))
+print("Media",mean(numeros_aleatorios))
+"""
+"""
+7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado termina con vocal, añadir un signo de exclamación al final e imprimir el string 
+resultante por pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por pantalla.
+
+cadena = input("Ingresa una palabra: ")
+vocal = cadena[-1]
+if vocal in "aeiouAeiou":
+    print(vocal + "!")
+else:
+    print(cadena)
+"""
+"""
+8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3 dependiendo de la opción que desee:
+1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el usuario e imprimir el resultado por pantalla. Nota: investigue uso 
+de las funciones upper(), lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+
+nombre=input("Ingrese su nombre: ")
+num= int(input("seleccione 1,2 o 3: "))
+if num == 1:
+    print(nombre.upper())
+elif num == 2:
+    print(nombre.lower())
+else:
+    print(nombre.title())
+"""
+"""
+9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la magnitud en una de las siguientes categorías 
+según la escala de Richter e imprima el resultado por pantalla:
+● Menor que 3: "Muy leve" (imperceptible).
+● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero generalmente no causa daños).
+● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras débiles).
+● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+richter= int(input("Ingrese un valor del 1 al 10 para clasificar el sismo: "))
+if richter <3 :
+    print("Muy leve (imperceptible).")
+elif richter >= 3 and richter < 4:
+    print( "Leve (ligeramente perceptible).")
+elif richter >= 4 and richter < 5:
+    print("Moderado (sentido por personas, pero generalmente no causa daños).")
+elif richter >= 5 and richter < 6:
+    print("Fuerte (puede causar daños en estructuras débiles).")
+elif richter >= 6 and richter < 7:
+    print("Muy Fuerte (puede causar daños significativos).")
+else:
+    print("Extremo (puede causar graves daños a gran escala).")
+"""
+"""
+10) Utilizando la información aportada en la siguiente tabla sobre las estaciones del año
+Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes del año es y qué día es. 
+El programa deberá utilizar esa información para imprimir por pantalla si el usuario se encuentra en otoño, invierno, primavera o verano.
+
+hemisferio= input("Se encuentra en el hemisferio Norte o Sur?: ")
+dia= int(input("Ingrese dìa del mes: "))
+mes= input("Ingrese el nombre del mes: ")
+
+if hemisferio.lower() == "norte" and ((mes.lower() == "diciembre" and dia >= 21) or (mes.lower() == "enero") or (mes.lower() == "febrero") or (mes.lower() == "marzo" and dia <= 20)):
+    print("Invierno")
+elif hemisferio.lower() == "norte" and ((mes.lower() == "marzo" and dia >= 21) or (mes.lower() == "abril") or (mes.lower() == "mayo") or (mes.lower() == "junio" and dia <= 20)):
+    print("Primavera")
+elif hemisferio.lower() == "norte" and ((mes.lower() == "junio" and dia >= 21) or (mes.lower() == "julio") or (mes.lower() == "agosto") or (mes.lower() == "septiembre" and dia <= 20)):
+    print("Verano")
+elif  hemisferio.lower() == "norte" and ((mes.lower() == "septiembre" and dia >= 21) or (mes.lower() == "octubre") or (mes.lower() == "noviembre") or (mes.lower() == "diciembre" and dia <= 20)):
+    print("Otoño")
+elif hemisferio.lower() == "sur" and ((mes.lower() == "diciembre" and dia >= 21) or (mes.lower() == "enero") or (mes.lower() == "febrero") or (mes.lower() == "marzo" and dia <= 20)):
+    print("Verano")
+elif hemisferio.lower() == "sur" and ((mes.lower() == "marzo" and dia >= 21) or (mes.lower() == "abril") or (mes.lower() == "mayo") or (mes.lower() == "junio" and dia <= 20)):
+    print("Otoño")
+elif hemisferio.lower() == "sur" and ((mes.lower() == "junio" and dia >= 21) or (mes.lower() == "julio") or (mes.lower() == "agosto") or (mes.lower() == "septiembre" and dia <= 20)):
+    print("Invierno")
+elif  hemisferio.lower() == "sur" and ((mes.lower() == "septiembre" and dia >= 21) or (mes.lower() == "octubre") or (mes.lower() == "noviembre") or (mes.lower() == "diciembre" and dia <= 20)):
+    print("Primavera")
+else:
+    print("Ingreso un valor equivocado, intente de nuevo")
+"""
